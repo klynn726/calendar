2c0097b33946631b99ba9ff38cc308727f80119f
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-03 16:25:50 -0700

    added time blocks to html

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
-var now = moment().format('[Today is] MM DD YYYY');
+var now = moment().format('[Today is] MM/DD/YYYY  [The time is] HH:MM');
+document.getElementById('currentDay').innerHTML = now;
 
-console.log (now);
+var before = moment().isBefore(now)
+document.getElementById('description')
+
+var after = moment().isAfter(now)
+document.getElementById('description')
+
+var same = moment().isSame(now)
+document.getElementById('description')
+
+
 
