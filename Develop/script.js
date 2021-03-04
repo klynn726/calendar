762d4c5cc26991e862afb7ace91f3fb2667e18a7
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-04 07:34:11 -0700

    deleted old method

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,8 @@
 var now = moment().format('[Today is] MM/DD/YYYY  [The time is] HH:MM');
 document.getElementById('currentDay').innerHTML = now;
 
-var before = moment().isBefore(now)
-document.getElementById('description')
+var timeNow = moment().hour();
 
-var after = moment().isAfter(now)
-document.getElementById('description')
-
-var same = moment().isSame(now)
-document.getElementById('description')
 
 
 
