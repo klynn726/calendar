d9ef62e8d0e64c6b9d2dd639db05693b8144e149
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-04 15:30:46 -0700

    added if else stmt for color

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,33 @@
-var now = moment().format('[Today is] MM/DD/YYYY  [The time is] HH:MM');
+var now = moment().format('[Today is] MM/DD/YYYY  [The time is] HH:mm');
 document.getElementById('currentDay').innerHTML = now;
-
-var timeNow = moment().hour();
-
+console.log(now);
 
 
+var timeNow = moment().format('HH')
+console.log(timeNow);
 
+
+//this is the documentation showing how to get the hour, day, and format
+//https://momentjs.com/docs/#/get-set/
+
+
+//if then for past, present, future and add class names
+
+//how to tell what time the block is associated with?
+//https://stackoverflow.com/questions/1735230/can-i-add-a-custom-attribute-to-an-html-tag#:~:text=Yes%2C%20you%20can%20do%20it!&text=')%3B%20%2F%2F%20Setter-,Element.,used%20for%20the%20current%20tag.
+//add a custom html attribute to the tag for the box you want to change the background color of
+
+//if timeNow is less than the hr attribute on the textarea tag then, else if greater than, and else equal add a class in each case
+
+if (timeNow < document.getElementById(".description").attr(".hr")) {
+  document.getElementById(".description").classList.add('.future');
+}
+//https://www.w3schools.com/jsref/prop_element_classlist.asp resource for adding classes dynamically
+
+else if (timeNow > document.getElementById(".description").attr("hr")) {
+  document.getElementById(".description").classList.add('.past');
+}
+
+else if (timeNow == document.getElementById(".description").attr("hr")) {
+  document.getElementById(".description").classList.add('.present');
+}
