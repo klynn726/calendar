e16654c9642133d651731a54943a569fd103935e
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-04 18:50:12 -0700

    added for loop

--- Develop/script.js
+++ Develop/script.js
@@ -11,11 +11,10 @@ console.log(timeNow);
 //https://momentjs.com/docs/#/get-set/
 
 
-//if then for past, present, future and add class names
-
 //how to tell what time the block is associated with?
 //https://stackoverflow.com/questions/1735230/can-i-add-a-custom-attribute-to-an-html-tag#:~:text=Yes%2C%20you%20can%20do%20it!&text=')%3B%20%2F%2F%20Setter-,Element.,used%20for%20the%20current%20tag.
 //add a custom html attribute to the tag for the box you want to change the background color of
+
 
 //if timeNow is less than the hr attribute on the
 //textarea tag then, else if greater than, and else equal add a class 
@@ -25,9 +24,13 @@ console.log(timeNow);
 // resource for adding classes dynamically
 
 function timeslot() {
-
+  
   var timeChunk = document.getElementsByClassName("description");
   var timeSlot = timeChunk.getAttritibute("hr");
+
+  //loop through the items in the timeSlot array
+
+  for (i = 0; i < timeChunk.length; i++){
 
   if (timeNow < timeSlot) {
     document.getElementsByClassName("description").classList.add("future");
@@ -38,6 +41,6 @@ function timeslot() {
   }
 
   else if (timeNow == timeSlot) {
-    document.getElementById("description").classList.add('present');
+    document.getElementById("description").classList.add("present");
   }
-}
+}}
